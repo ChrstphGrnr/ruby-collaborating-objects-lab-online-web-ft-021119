792aedeb6f3817adffbaabec2750b7d47b052cd8
Christoph Gruener <christophgruener@me.com>
2019-03-05 00:16:53 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -5,10 +5,14 @@ class MP3Importer
 
   def initialize(file_path)
     @path = file_path
+<<<<<<< HEAD
     # binding.pry
     @file_names = Dir.entries(@path).keep_if{|item| item.end_with?(".mp3")}
     # @file_names.pop
     # @file_names.shift
+=======
+    @file_names = []
+>>>>>>> 172646711cd329c9e6233e4396cdd5bd9312f8b1
   end
 
   def files
@@ -33,8 +37,11 @@ class MP3Importer
 
 
 end
+<<<<<<< HEAD
 
 # def initialize(path)
 #     @path = path
 #     @files = Dir.entries(path).select {|f| f[-3..-1] == "mp3"}
 #   end
+=======
+>>>>>>> 172646711cd329c9e6233e4396cdd5bd9312f8b1
